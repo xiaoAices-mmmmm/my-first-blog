3ceed0210ba48fbe87c9f32a79cf573f7669cf38
zhm <17695441231@163.com>
2025-08-07 15:24:22 +0800

    tijiao

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
-from django.urls.conf import path
+from django.urls.conf import path,re_path
 from . import views
 
 urlpatterns = [
-    path('post_list/',views.post_list,name='post_list')
+    path('post_list/',views.post_list,name='post_list'),
+    # path('post/<int:>/',views.get_num,name='post_detail')
+    re_path(r'^post/(?P<p>\d+)/$',views.get_num,name='get_num')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,get_object_or_404
 from .models import Post
 from django.utils import timezone
 # Create your views here.
@@ -6,5 +6,11 @@ from django.utils import timezone
 def post_list(request):
     # d = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     d = Post.objects.all()
-    return render(request,'blog/post_list.html',{'data':d})
+    return render(request,'blog/post_list.html',context={'polist':d})
+def post_detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request,'blog/post_detail.html',{'detail':post})
 
+def get_num(request,p):
+    print(p)
+    return HttpResponse(p)
